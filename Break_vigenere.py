5ad20cd616226a3246224cae3edd432187bbe99b
kunj bhesaniya <kcbhesaniya10@gmail.com>
2024-09-28 15:36:19 +0530

    minor changes

--- Break_vigenere.py
+++ Break_vigenere.py
@@ -1,6 +1,6 @@
 import string
 from collections import Counter
-import Key_length_estimate
+from Key_length_estimate import estimate_key_length
 # Letter frequency in English language
 ENGLISH_FREQ = {
     'A': 8.167, 'B': 1.492, 'C': 2.782, 'D': 4.253, 'E': 12.702, 'F': 2.228,
